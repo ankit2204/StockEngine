926decb7f243fb0f61adfcdb2b4ea6473433a3c6
ankit2204 <ankitagarwal2204@gmail.com>
2017-02-27 02:08:40 +0530

    added demo get request

--- src/main/java/com/stockengine/controller/ScriptController.java
+++ src/main/java/com/stockengine/controller/ScriptController.java
@@ -8,6 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/script")
 public class ScriptController {
 	
+	private static final String info = "Hello this is Stock analysis application in progress";
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String getInfo(){
+		return info;
+	}
+	
 	@RequestMapping(method = RequestMethod.POST)
 	public void addScript(){
 		
